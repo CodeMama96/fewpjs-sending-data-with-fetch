01774e89ac2fc9a0ba26c1f622dd277cdef5363c
Alicia Weenum <weenum.alicia@gmail.com>
2021-02-04 19:21:52 -0500

    commit tree 32810acc3f16e27672d0172e091ec94b45f62430

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Add your code here
+//fetch(destinationURL, configurationObject); 
+
+  //"Content-Type" is used to indicate what format the data being sent is in.
+
+  let formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle"
+  };
+  
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+  
+  fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  }); 
+
+    // body: /* Your data goes here */ needs to be a string
+
+    let submitData = {
+        userName: "Steve",
+        userEmail: "steve@steve.com"
+      };
+
+      let userObj = {
+        //method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(submitData)
+      };
+      
+      fetch("http://localhost:3000/users", userObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        console.log(object);
+      })
+      .catch(function(error) {
+        alert("Bad things!");
+        console.log(error.message);
+      }); 
+
+    
